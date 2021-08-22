b8b0643d9167fa87fc40bcf5edcba521c664baa8
Gilbert Gambin <gilbertgambin@gmail.com>
2021-08-22 10:48:40 +0200

    add custom scss

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/styles/main.scss'
 
 createApp(App).use(store).use(router).mount('#app')
